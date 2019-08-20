e7438271cc4f7f3304878de0eaec77c9563ad7fc
“Atif <atif.gcucs@gmail.com>
2019-08-20 15:03:05 +0400

    Bidi Text

--- BidirectionalTextSwift/BidirectionalTextSwift/ViewController.swift
+++ BidirectionalTextSwift/BidirectionalTextSwift/ViewController.swift
@@ -13,14 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let attStr = NSAttributedString(string: lblText.text!, attributes: [.foregroundColor : UIColor.white, .writingDirection: [NSWritingDirection.rightToLeft.rawValue]])
-        lblText.attributedText = attStr
+//        let attStr = NSAttributedString(string: lblText.text!, attributes: [.foregroundColor : UIColor.white, .writingDirection: [NSWritingDirection.rightToLeft.rawValue]])
+//        lblText.attributedText = attStr
 
 
         let str = "\"STC Atif Naveed\" تعلن عدم الاستمرار في اتفاقية نقل ورعاية الدوري السعودي \"لعدم جدواها تجاريا\""
-//        let subAttr: [NSAttributedString.Key : Any] = [.writingDirection : [ NSNumber(value: NSWritingDirection.rightToLeft.rawValue)]]
-//        let attStr = NSAttributedString(string: str, attributes: subAttr)
-//        lblText.attributedText = attStr
+        let subAttr: [NSAttributedString.Key : Any] = [.writingDirection : [ NSNumber(value: NSWritingDirection.rightToLeft.rawValue)]]
+        let attStr = NSAttributedString(string: str, attributes: subAttr)
+        lblText.attributedText = attStr
         
         
         for scalar in str.unicodeScalars {
