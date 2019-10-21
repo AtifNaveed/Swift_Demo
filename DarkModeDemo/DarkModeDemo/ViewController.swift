87422b13df48b70f22d7c4d2da518bd58c343fff
“Atif <atif.gcucs@gmail.com>
2019-10-21 18:17:23 +0400

    DarkMode Demo

---
+++ DarkModeDemo/DarkModeDemo/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  DarkModeDemo
+//
+//  Created by Atif on 21/10/2019.
+//  Copyright © 2019 Atif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHelloWorld: UILabel!
+    @IBOutlet var bg: UIView!
+    
+    let dynamicColor = UIColor { (traitCollection: UITraitCollection) -> UIColor in
+        switch traitCollection.userInterfaceStyle {
+        case
+          .unspecified,
+          .light: return .white
+        case .dark:
+            return .black
+        @unknown default:
+            return .white
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bg.backgroundColor = dynamicColor
+        
+        lblHelloWorld.textColor = .label
+    }
+
+    
+
+}
+
+
+
